f91d0bac9f0398e5832b488e4b498c48e1bebeb3
Cameron Ross <elpenguino@gmail.com>
2020-11-30 23:35:23 -0400

    implement struct support

--- source/reversineer.d
+++ source/reversineer.d
@@ -165,36 +165,60 @@ pure @safe nothrow unittest {
 
 private struct EndianType(T, bool littleEndian) {
 	ubyte[T.sizeof] raw;
-	alias toInt this;
-	T toInt() const {
-		import std.bitmanip : bigEndianToNative, littleEndianToNative;
-		if (__ctfe) {
-			return 0;
+	alias native this;
+	version(BigEndian) {
+		enum needSwap = littleEndian;
+	} else {
+		enum needSwap = !littleEndian;
+	}
+	T native() const @safe {
+		T result = (cast(T[])(raw[].dup))[0];
+		static if (needSwap) {
+			swapEndianness(result);
 		}
-		static if (littleEndian) {
-			return littleEndianToNative!T(raw);
-		} else {
-			return bigEndianToNative!T(raw);
-		}
+		return result;
 	}
 	void toString(Range)(Range sink) const if (isOutputRange!(Range, const(char))) {
 		import std.format : formattedWrite;
-		sink.formattedWrite!"%s"(this.toInt());
+		sink.formattedWrite!"%s"(this.native);
 	}
 	void opAssign(ubyte[T.sizeof] input) {
 		raw = input;
 	}
 	void opAssign(ubyte[] input) {
+		assert(input.length == T.sizeof, "Array must be "~T.sizeof.stringof~" bytes long");
 		raw = input;
 	}
-	void opAssign(const T input) {
-		import std.bitmanip : nativeToBigEndian, nativeToLittleEndian;
-		static if (littleEndian) {
-			raw = nativeToLittleEndian!T(input);
-		} else {
-			raw = nativeToBigEndian!T(input);
+	void opAssign(T input) @safe {
+		static if (needSwap) {
+			swapEndianness(input);
 		}
+		union Raw {
+			T val;
+			ubyte[T.sizeof] raw;
+		}
+		raw = Raw(input).raw;
 	}
+}
+
+void swapEndianness(T)(ref T val) {
+	import std.bitmanip : swapEndian;
+	static if (isIntegral!T || isSomeChar!T || isBoolean!T) {
+		val = swapEndian(val);
+	} else static if (isFloatingPoint!T) {
+		import std.algorithm : reverse;
+		union Raw {
+			T val;
+			ubyte[T.sizeof] raw;
+		}
+		auto raw = Raw(val);
+		reverse(raw.raw[]);
+		val = raw.val;
+	} else static if (is(T == struct)) {
+		foreach (ref field; val.tupleof) {
+			swapEndianness(field);
+		}
+	} else static assert(0, "Unsupported type "~T.stringof);
 }
 
 /++
@@ -215,6 +239,22 @@ alias LittleEndian(T) = EndianType!(T, true);
 	assert(x.text == "768");
 	x = 1024;
 	assert(x.raw == [0, 4]);
+
+	LittleEndian!float f;
+	f = cast(ubyte[])[0, 0, 32, 64];
+	assert(f == 2.5);
+
+	align(1) static struct Test {
+		align(1):
+		uint a;
+		ushort b;
+	}
+	LittleEndian!Test t;
+	t = cast(ubyte[])[3, 2, 1, 0, 2, 1];
+	assert(t.a == 0x010203);
+	assert(t.b == 0x0102);
+	t = Test(42, 42);
+	assert(t.raw == [42, 0, 0, 0, 42, 0]);
 }
 
 /++
@@ -231,6 +271,22 @@ alias BigEndian(T) = EndianType!(T, false);
 	assert(x.text == "512");
 	x = 1024;
 	assert(x.raw == [4, 0]);
+
+	BigEndian!float f;
+	f = cast(ubyte[])[64, 32, 0, 0];
+	assert(f == 2.5);
+
+	align(1) static struct Test {
+		align(1):
+		uint a;
+		ushort b;
+	}
+	BigEndian!Test t;
+	t = cast(ubyte[])[0, 1, 2, 3, 1, 2];
+	assert(t.a == 0x010203);
+	assert(t.b == 0x0102);
+	t = Test(42, 42);
+	assert(t.raw == [0, 0, 0, 42, 0, 42]);
 }
 
 private T fromPBCD(T)(ubyte[T.sizeof] input) {
